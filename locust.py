964fd7fd527fd6f29274ae8d1b74cd4c53cfc9ea
Raghu <72290009+ry-v1@users.noreply.github.com>
2022-10-27 11:03:51 +0530

    Update locust.py

--- locust.py
+++ locust.py
@@ -1,5 +1,25 @@
 from locust import HttpUser, task
-from data_test import input_data
+
+input_data = {
+   "CHAS":{
+      "0":0
+   },
+   "RM":{
+      "0":6.575
+   },
+   "TAX":{
+      "0":296.0
+   },
+   "PTRATIO":{
+      "0":15.3
+   },
+   "B":{
+      "0":396.9
+   },
+   "LSTAT":{
+      "0":4.98
+   }
+}
 
 class LocustTaskSet(HttpUser):
 
